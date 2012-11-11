7e59aa4e3ad40c944f4c79283c95e4a9783a2e29
nmred <nmred_2008@126.com>
2012-11-11 22:43:05 +0800

    add dispatcher

---
+++ src/lib/ui/action/user/sw_controller_user_base_action.class.php
@@ -0,0 +1,60 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 foldmethod=marker: */
+// +---------------------------------------------------------------------------
+// | SWAN [ $_SWANBR_SLOGAN_$ ]
+// +---------------------------------------------------------------------------
+// | Copyright $_SWANBR_COPYRIGHT_$
+// +---------------------------------------------------------------------------
+// | Version  $_SWANBR_VERSION_$
+// +---------------------------------------------------------------------------
+// | Licensed ( $_SWANBR_LICENSED_URL_$ )
+// +---------------------------------------------------------------------------
+// | $_SWANBR_WEB_DOMAIN_$
+// +---------------------------------------------------------------------------
+ 
+/**
++------------------------------------------------------------------------------
+* 用户端首页
++------------------------------------------------------------------------------
+* 
+* @package 
+* @version $_SWANBR_VERSION_$
+* @copyright $_SWANBR_COPYRIGHT_$
+* @author $_SWANBR_AUTHOR_$ 
++------------------------------------------------------------------------------
+*/
+class sw_controller_user_base_action
+{
+	// {{{ members
+	// }}}
+	// {{{ functions
+	// {{{ public function action_default()
+
+	/**
+	 * action_default 
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function action_default()
+	{
+		
+	}
+
+	// }}}
+	// {{{ public function action_do()
+
+	/**
+	 * action_do
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function action_do()
+	{
+		
+	}
+
+	// }}}
+	// }}}
+}
