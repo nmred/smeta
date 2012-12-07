afeccbe5b6e0d9861f0fd85a5eadc0bcf3574626
nmred <nmred_2008@126.com>
2012-12-08 07:03:37 +0800

    mod

--- src/core.php
+++ src/core.php
@@ -48,7 +48,7 @@ define('PATH_SNMP_BIN', '/usr/bin/');
 define('SWAN_SOFTNAME', 'swansoft');
 
 // 软件版本号
-define('SWAN_VERSION', '0.12.b');
+define('SWAN_VERSION', '0.12.c');
 
 // 软件发行号
 define('SWANBR_RELEASE', 'beta');
