e881c8866720e317da6e092e583a8e96d3226ff5
nmred <nmred_2008@126.com>
2013-06-25 19:59:47 +0800

    add

--- src/lib/db/statement/sw_abstract.php
+++ src/lib/db/statement/sw_abstract.php
@@ -58,6 +58,22 @@ abstract class sw_abstract
 	 */
 	protected $__stmt = null;
 
+	/**
+	 * 绑定的参数 
+	 * 
+	 * @var array
+	 * @access protected
+	 */
+	protected $__bind_param = array();
+
+	/**
+	 * 获取数据集模式 
+	 * 
+	 * @var mixed
+	 * @access protected
+	 */
+	protected $__fetch_mode = PDO::FETCH_ASSOC;
+
 	// }}}
 	// {{{ functions
 	// {{{ public function __construct()
@@ -190,5 +206,224 @@ abstract class sw_abstract
 	}
 
 	// }}}
+	// {{{ public function execute()
+
+	/**
+	 * 执行 sql 语言 
+	 * 
+	 * @param array $params 
+	 * @access public
+	 * @return boolean
+	 */
+	public function execute(array $params = null)
+	{
+		if ($this->__query_id === null) {
+			return $this->_execute($params);	
+		}
+		
+		$prof = $this->__adapter->get_profiler();
+		$qp = $prof->get_query_profile($this->__query_id);
+		if ($qp->has_ended()) {
+			$this->__query_id = $prof->query_clone($qp);	
+			$qp = $prof->get_query_profile($this->__query_id);
+		}
+
+		if ($params !== null) {
+			$qp->bind_param($params);	
+		} else {
+			$qp->bind_param($this->__bind_param);	
+		}
+
+		$qp->start($this->__query_id);
+
+		$retval = $this->_execute($params);
+
+		$prof->query_end($this->__query_id);
+
+		return $retval;
+	}
+
+	// }}}
+	// {{{ public function fetch()
+
+	/**
+	 * 获取数据集 
+	 * 
+	 * @param mixed $style 
+	 * @param mixed $offset 
+	 * @access public
+	 * @return void
+	 */
+	public function fetch($style = null; $cursor = null, $offset = null)
+	{
+		if ($style === null) {
+			$style = $this->__fetch_mode;	
+		}
+
+		try {
+			return $this->__stmt->fetch($style, $cursor, $offset);	
+		} catch (PDOException $e) {
+			throw new sw_exception($e->getMessage(), $e->getCode(), $e);
+		}
+	}
+
+	// }}}
+	// {{{ public function fetch_all()
+
+	/**
+	 * 遍历所有的数据集 
+	 * 
+	 * @param mixed $style 
+	 * @param mixed $col 
+	 * @access public
+	 * @return array
+	 */
+	public function fetch_all($style = null, $col = null)
+	{
+		if ($style === null) {
+			$style = $this->__fetch_mode;	
+		}
+
+		try {
+			if ($style == PDO::FETCH_COLUMN) {
+				if ($col === null) {
+					$col = 0;	
+				}	
+
+				return $this->__stmt->fetchAll($style, $col);
+			} else {
+				return $this->__stmt->fetchAll($style);	
+			}
+		} catch (PDOException $e) {
+			throw new sw_exception($e->getMessage(), $e->getCode(), $e);	
+		}
+	}
+
+	// }}}
+	// {{{ public function fetch_column()
+	
+	/**
+	 * 获取一行中的指定的一列 
+	 * 
+	 * @param int $col 
+	 * @access public
+	 * @return string
+	 */
+	public function fetch_column($col = 0)
+	{
+		try {
+			return $this->__stmt->fetchColumn($col);	
+		} catch (PDOException $e) {
+			throw new sw_exception($e->getMessage(), $e->getCode(), $e);
+		}
+	}
+
+	// }}}
+	// {{{ public function get_attribute()
+
+	/**
+	 * 获取属性值 
+	 * 
+	 * @param string $key 
+	 * @access public
+	 * @return mixed
+	 */
+	public function get_attribute($key)
+	{
+		try {
+			return $this->__stmt->getAttribute($key);	
+		} catch (PDOException $e) {
+			throw new sw_exception($e->getMessage(), $e->getCode(), $e);
+		}
+	}
+
+	// }}}
+	// {{{ public function set_attribute()
+
+	/**
+	 * 设置属性值 
+	 * 
+	 * @param string $key 
+	 * @param mixed $val 
+	 * @access public
+	 * @return boolean
+	 */
+	public function set_attribute($key, $val)
+	{
+		try {
+			return $this->__stmt->setAttribute($key, $val);	
+		} catch (PDOException $e) {
+			throw new sw_exception($e->getMessage(), $e->getCode(), $e);
+		}
+	}
+
+	// }}}
+	// {{{ public function set_fetch_mode()
+
+	/**
+	 * 设置 fetch 的模式 
+	 * 
+	 * @param integer $mode 
+	 * @access public
+	 * @return void
+	 */
+	public function set_fetch_mode($mode)
+	{
+		$this->__fetch_mode = $mode;
+		try {
+			return $this->__stmt->setFetchMode($mode);
+		} catch (PDOException $e) {
+			throw new sw_exception($e->getMessage(), $e->getCode(), $e);
+		}
+	}
+
+	// }}}
+	// {{{ public function get_adapter()
+
+	/**
+	 * 获取适配器对象 
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function get_adapter()
+	{
+		return $this->__adapter;	
+	}
+
+	// }}}
+	// {{{ public function get_stmt()
+
+	/**
+	 * 获取 stmt 对象 
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function get_stmt()
+	{
+		return $this->__stmt;	
+	}
+
+	// }}}
+	// {{{ public function column_count()
+
+	/**
+	 * 获得结果集的记录字段数 
+	 * 
+	 * @access public
+	 * @return integer
+	 */
+	public function column_count()
+	{
+		try {
+			return $this->__stmt->columnCount();	
+		} catch (PDOException $e) {
+			throw new sw_exception($e->getMessage(), $e->getCode(), $e);
+		}
+	}
+
+	// }}}
+	// {{{ 
 	// }}}
 }
