5dab582ca05fe3a7084621156dbf05e850e88357
nmred <nmred_2008@126.com>
2014-01-05 19:42:49 +0800

    mod phpd

--- src/lib/process/sw_test.class.php
+++ src/lib/process/sw_test.class.php
@@ -1,37 +1,63 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 foldmethod=marker: */
+// +---------------------------------------------------------------------------
+// | SWAN [ $_SWANBR_SLOGAN_$ ]
+// +---------------------------------------------------------------------------
+// | Copyright $_SWANBR_COPYRIGHT_$
+// +---------------------------------------------------------------------------
+// | Version  $_SWANBR_VERSION_$
+// +---------------------------------------------------------------------------
+// | Licensed ( $_SWANBR_LICENSED_URL_$ )
+// +---------------------------------------------------------------------------
+// | $_SWANBR_WEB_DOMAIN_$
+// +---------------------------------------------------------------------------
 
 namespace lib\process;
-
-class sw_test
+ 
+/**
++------------------------------------------------------------------------------
+* sw_test 
++------------------------------------------------------------------------------
+* 
+* @package 
+* @version $_SWANBR_VERSION_$
+* @copyright $_SWANBR_COPYRIGHT_$
+* @author $_SWANBR_AUTHOR_$ 
++------------------------------------------------------------------------------
+*/
+class sw_test extends sw_abstract
 {
-	protected $__log;
-	protected $__message;
-	public function set_log($log)
-	{
-		$this->__log = $log;	
-	}
+	// {{{ functions
+	// {{{ protected function _init()
 
-	public function run()
-	{
-		while(1) {
-			$this->log("test ....................", LOG_DEBUG);
-			sleep(1);	
-		}	
-	}
-
-	public function set_proc_config($config)
+	/**
+	 * 进程初始化 
+	 * 
+	 * @access protected
+	 * @return void
+	 */
+	protected function _init()
 	{
 		
 	}
 
-	public function log($message, $level) 
+	// }}}
+	// {{{ protected function _run()
+
+	/**
+	 * 进程运行 
+	 * 
+	 * @access protected
+	 * @return void
+	 */
+	protected function _run()
 	{
-		$this->__message->message = $message;
-		$this->__log->log($this->__message, $level);	
+		while (1) {
+			$this->log("runing ...... time:" . time(), LOG_DEBUG);	
+			sleep(1);
+		}
 	}
 
-	public function set_message($message)
-	{
-		$this->__message = $message;	
-	}
+	// }}}
+	// }}}
 }
