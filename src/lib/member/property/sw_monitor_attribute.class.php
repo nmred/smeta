741a3335377be51db08c1c66cde09ed824d7a06e
nmred <nmred_2008@126.com>
2014-01-12 23:22:03 +0800

    add monitor attribute

---
+++ src/lib/member/property/sw_monitor_attribute.class.php
@@ -0,0 +1,74 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 foldmethod=marker: */
+// +---------------------------------------------------------------------------
+// | SWAN [ $_SWANBR_SLOGAN_$ ]
+// +---------------------------------------------------------------------------
+// | Copyright $_SWANBR_COPYRIGHT_$
+// +---------------------------------------------------------------------------
+// | Version  $_SWANBR_VERSION_$
+// +---------------------------------------------------------------------------
+// | Licensed ( $_SWANBR_LICENSED_URL_$ )
+// +---------------------------------------------------------------------------
+// | $_SWANBR_WEB_DOMAIN_$
+// +---------------------------------------------------------------------------
+ 
+namespace lib\member\property;
+use \lib\member\property\exception\sw_exception;
+
+/**
++------------------------------------------------------------------------------
+* sw_monitor_attribute 
++------------------------------------------------------------------------------
+* 
+* @uses sw_abstract
+* @package 
+* @version $_SWANBR_VERSION_$
+* @copyright $_SWANBR_COPYRIGHT_$
+* @author $_SWANBR_AUTHOR_$ 
++------------------------------------------------------------------------------
+*/
+class sw_monitor_attribute extends sw_abstract
+{
+	// {{{ members
+
+	/**
+	 * 允许设置的元素列表 
+	 * 
+	 * @var array
+	 * @access protected
+	 */
+	protected $__allow_attributes = array(
+		'attr_id'           => true,
+		'monitor_id'        => true,
+		'attr_name_name'    => true,
+		'attr_display_name' => true,
+		'form_type'         => true,
+		'form_data'         => true,
+	);
+
+	/**
+	 * 主键 
+	 * 
+	 * @var string
+	 * @access protected
+	 */
+	protected $__key_attributes = array('monitor_id', 'attr_id');
+
+	// }}}		
+	// {{{ functions
+	// {{{ public function check()
+
+	/**
+	 * 检查参数 
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function check()
+	{
+		parent::check();
+	}
+
+	// }}}
+	// }}}
+}
