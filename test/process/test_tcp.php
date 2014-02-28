daad067ac66a520f80024b0474c949b5ab088bcd
nmred <nmred_2008@126.com>
2014-03-01 01:55:33 +0800

    mod init data xml

--- test/process/test_tcp.php
+++ test/process/test_tcp.php
@@ -1,10 +1,20 @@
 <?php
-$fp = stream_socket_client("tcp://127.0.0.1:8649", $errno, $errstr);
-if (!$fp) {
-	echo "ERROR: $errno - $errstr<br />\n";
+//$fp = stream_socket_client("tcp://127.0.0.1:8649", $errno, $errstr);
+//if (!$fp) {
+//	echo "ERROR: $errno - $errstr<br />\n";
+//} else {
+//	fwrite($fp, "\n");
+//	echo fread($fp, 26);
+//	fclose($fp);
+//}
+$socket = stream_socket_server("tcp://0.0.0.0:8649", $errno, $errstr);
+if (!$socket) {
+  echo "$errstr ($errno)<br />\n";
 } else {
-	fwrite($fp, "\n");
-	echo fread($fp, 26);
-	fclose($fp);
+  while ($conn = stream_socket_accept($socket)) {
+echo stream_socket_recvfrom($conn, 4096);
+    fclose($conn);
+  }
+  fclose($socket);
 }
 ?>
