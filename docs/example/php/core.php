5dd04109fc3d1038799fb2a87c4695d951602b79
nmred <nmred_2008@126.com>
2014-04-08 15:34:54 +0800

    mod

--- docs/example/php/core.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-define('SW_TEST_BASE', '../../../src/');
-
-require_once SW_TEST_BASE . 'core.php';
--- docs/example/php/db/test_db_expr.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'core.php';
-use lib\db\sw_db_expr;
-
-$expr = new lib\db\sw_db_expr('a = a + 1');
-
-P($expr);
-echo $expr;
